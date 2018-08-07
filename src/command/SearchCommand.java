ad832ba833471b63242cc1bbbf16c4e99110dcef
김승태 <39721124+kosoto@users.noreply.github.com>
2018-08-07 20:20:15 +0900

    Update SearchCommand.java

--- src/command/SearchCommand.java
+++ src/command/SearchCommand.java
@@ -23,7 +23,16 @@ public class SearchCommand extends Command{
 					request.getParameter("option")+"/"+
 					request.getParameter("word"));
 		}else {//전체 리스트 코딩 영역
-			int pageNum = (request.getParameter("pageNum")==null)?1:Integer.parseInt(request.getParameter("pageNum")), count = MemberServiceImpl.getInstance().countMember(), pageSize = 5, blockSize = 5, beginPage = Math.floorDiv(pageNum-1, pageSize)*pageSize+1, endPage = (count>(beginPage+(blockSize-1))*pageSize)?beginPage+(blockSize-1):(int)(Math.ceil(count/(double)pageSize)), beginRow = (pageNum-1)*pageSize+1, endRow= (count>pageNum*pageSize)?pageNum*pageSize:count;
+			int pageNum = (request.getParameter("pageNum")==null)?
+				1:Integer.parseInt(request.getParameter("pageNum")), 
+			count = MemberServiceImpl.getInstance().countMember(), 
+			pageSize = 5, 
+			blockSize = 5, 
+			beginPage = Math.floorDiv(pageNum-1, pageSize)*pageSize+1, 
+			endPage = (count>(beginPage+(blockSize-1))*pageSize)?
+				beginPage+(blockSize-1):(int)(Math.ceil(count/(double)pageSize)), 
+			beginRow = (pageNum-1)*pageSize+1, 
+			endRow= (count>pageNum*pageSize)?pageNum*pageSize:count;
 			Map<String,Object>param = new HashMap<>();
 			param.put("beginRow", String.valueOf(beginRow));
 			param.put("endRow", String.valueOf(endRow));
